4b76bb996e382013e7f11a95183e9ecbe6196435
Oscar Carlsson <oscar.carlsson@gmail.com>
2017-12-08 14:59:38 +0100

    Changed NO_QUOT to be a quote, and introduced NO_UMLT.
    Nordic layouts have a umlaut/tilde/circ key where KC_RBRC normally
    resides, while KC_QUOT is used for Ø/Ö, and the quote key is moved to
    KC_BSLS. The Nordic umlaut key was incorrectly labeled as NO_QUOT
    which made no sense considering it's function, and we also lacked a
    true NO_QUOT key. This commit fixes both of these issues.
    
    Also made the use of tabs/spaces consistent for this file.

--- quantum/keymap_extras/keymap_nordic.h
+++ quantum/keymap_extras/keymap_nordic.h
@@ -23,11 +23,12 @@
 #define NO_PLUS KC_MINS
 #define NO_ACUT KC_EQL
 
-#define NO_AM KC_LBRC
-#define NO_QUOT KC_RBRC  // this is the "umlaut" char on Nordic keyboards, Apple layout
-#define NO_AE KC_SCLN
+#define NO_AM   KC_LBRC
+#define NO_UMLT KC_RBRC // this is the "umlaut" char on Nordic keyboards, Apple layout
+#define NO_AE   KC_SCLN
 #define NO_OSLH KC_QUOT
 #define NO_APOS KC_NUHS
+#define NO_QUOT KC_BSLS // quote key is on the backslash key on a US keyboard
 
 #define NO_LESS KC_NUBS
 #define NO_MINS KC_SLSH
@@ -40,11 +41,12 @@
 #define NO_SLSH LSFT(KC_7)
 #define NO_LPRN LSFT(KC_8)
 #define NO_RPRN LSFT(KC_9)
-#define NO_EQL LSFT(KC_0)
+#define NO_EQL  LSFT(KC_0)
 #define NO_QUES LSFT(NO_PLUS)
-#define NO_GRV LSFT(NO_ACUT)
+#define NO_GRV  LSFT(NO_ACUT)
 
-#define NO_CIRC LSFT(NO_QUOT)
+#define NO_ASTR LSFT(NO_QUOT)
+#define NO_CIRC LSFT(NO_UMLT)
 
 #define NO_GRTR LSFT(NO_LESS)
 #define NO_SCLN LSFT(KC_COMM)
@@ -52,9 +54,9 @@
 #define NO_UNDS LSFT(NO_MINS)
 
 // Alt Gr-ed characters
-#define NO_AT ALGR(KC_2)
-#define NO_PND ALGR(KC_3)
-#define NO_DLR ALGR(KC_4)
+#define NO_AT   ALGR(KC_2)
+#define NO_PND  ALGR(KC_3)
+#define NO_DLR  ALGR(KC_4)
 #define NO_LCBR ALGR(KC_7)
 #define NO_LBRC ALGR(KC_8)
 #define NO_RBRC ALGR(KC_9)
@@ -62,9 +64,9 @@
 #define NO_PIPE ALGR(KC_NUBS)
 
 #define NO_EURO ALGR(KC_E)
-#define NO_TILD ALGR(NO_QUOT)
+#define NO_TILD ALGR(NO_UMLT)
 
 #define NO_BSLS ALGR(KC_MINS)
-#define NO_MU ALGR(KC_M)
+#define NO_MU   ALGR(KC_M)
 
 #endif
